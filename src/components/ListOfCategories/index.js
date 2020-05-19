352d8955a76aae9ff806f9757f70abcb7e0f9d6c
Palumbo <fcojnavarrowork@gmail.com>
2020-05-19 13:42:24 +0200

    -

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Category } from '../Category';
 
 import { List, Item } from './styles';
-import { categories } from '../../../api/db.json';
+import { categories as mockCategories } from '../../../api/db.json';
 
 export const ListOfCategories = () => {
+    const [categories, setCategories] = useState(mockCategories);
+    
     return (
         <List>
             {
