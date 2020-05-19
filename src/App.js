4d87b2c19aacef4f22731ba0cb57fea6ed15e1e2
Palumbo <fcojnavarrowork@gmail.com>
2020-05-19 10:47:06 +0200

    Añadido logo y animacion a PhotoCard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { ListOfCategories } from './components/ListOfCategories';
-import { GlobalStyle } from './GlobalStyle';
+import { GlobalStyle } from './styles/GlobalStyle';
 import { ListOfPhotoCards } from './components/ListOfPhotoCard';
+import { Logo } from './components/Logo';
 
 export const App = () => (
     <React.Fragment>
         <GlobalStyle />
+        <Logo />
         <ListOfCategories />
         <ListOfPhotoCards />
     </React.Fragment>
