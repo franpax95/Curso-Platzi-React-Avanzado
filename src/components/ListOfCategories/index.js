77a453f5fa2b0bc2e7d66b8a720c16d4c273df42
Palumbo <fcojnavarrowork@gmail.com>
2020-05-19 16:47:32 +0200

    Cambiando componentes a componentes funcionales con hooks personalizados y uso de Intersection Observer

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,19 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Category } from '../Category';
 
 import { List, Item } from './styles';
 import { categories as mockCategories } from '../../../api/db.json';
 
+function useCategoriesData(){
+    const [categories, setCategories] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        window.fetch('https://curso-platzi-react-avanzado-h10w64ocw.now.sh/categories')
+            .then(res => res.json())
+            .then(response => {
+                setCategories(response);
+                //add some delay (2s) to see the loading
+                setTimeout(() => setLoading(false), 2000);
+            });
+    }, []);
+
+    return { categories, loading };
+}
+
 export const ListOfCategories = () => {
-    const [categories, setCategories] = useState(mockCategories);
-    
-    return (
-        <List>
+    const [showFixed, setShowFixed] = useState(false);
+    const { categories, loading } = useCategoriesData();
+
+    useEffect(() => {
+        const onScroll = e => {
+            const newShowFixed = window.scrollY > 200;
+            showFixed !== newShowFixed && setShowFixed(newShowFixed);
+        }
+        document.addEventListener('scroll', onScroll);
+
+        return () => document.removeEventListener('scroll', onScroll);
+    });
+
+    const renderList = (fixed) => (
+        <List fixed={fixed}>
             {
-                categories.map((category) => (
-                    <Item key={category.id}><Category {...category} /></Item>
-                ))
+                (loading) 
+                    ? <Item key='loading'><Category /></Item>
+                    : categories.map((category) => (
+                        <Item key={category.id}><Category {...category} /></Item>
+                      ))
             }
         </List>
+    )
+
+    return (
+        <React.Fragment>
+            {renderList()}
+            {showFixed && renderList(true)}
+        </React.Fragment>
     )
 }
